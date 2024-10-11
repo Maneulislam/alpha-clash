fb1c53e75c5e5e57250c5c5e176feea0bea3f082
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2024-10-11 17:20:50 +0600

    Complete

--- script.js
+++ script.js
@@ -6,10 +6,16 @@ function hiddenId(elementId){
     
 }
 
+
+
+
 function showId(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+
+
 
 
 function getAlphabet(){
@@ -25,10 +31,95 @@ function getAlphabet(){
 }
 
 
+
+
+
 function setBackgroundColor(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+
+
+
+
+function removeBackgroundColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+function getElement(elementId){
+    const element = document.getElementById(elementId);
+        const elementText = element.innerText;
+        const score = parseInt(elementText);
+        return score;
+}
+
+
+function setElement(elementId , score){
+    const element = document.getElementById(elementId);
+    element.innerText = score;
+    
+}
+
+
+
+function getElementText(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
+
+
+
+
+document.addEventListener('keyup', handleKeyboard);
+
+
+function handleKeyboard(event){
+    const playerPressed = event.key;
+    console.log(playerPressed);
+
+
+    const currentKey = document.getElementById('alphabet-screen');
+    const alphabet = currentKey.innerText;
+    const expectedAlphabet = alphabet.toLowerCase();
+
+
+
+    if(expectedAlphabet === playerPressed){
+        console.log('you win')
+
+        
+
+
+        const currentScore = getElement('score');
+        const updatedScore = currentScore + 1;
+        setElement('score', updatedScore);
+
+        
+        
+        removeBackgroundColor(playerPressed);
+        continueGame();
+    }
+    else{
+        console.log('you loss')
+
+        const currentLife = getElement('life');
+        const updatedLife = currentLife - 1;
+        setElement('life', updatedLife);
+
+
+       if(updatedLife === 0){
+        gameOver();
+       }
+
+
+    }
+}
+
+
+
 
 
 
@@ -48,8 +139,32 @@ function continueGame(){
 
 
 
+
+
 function play(){
     hiddenId('first-page');
+    hiddenId('third-page');
     showId('second-page');
+
+
+    setElement('life',5);
+    setElement('score',0);
+
+
     continueGame();
+}
+
+
+
+function gameOver(){
+    hiddenId('second-page');
+    showId('third-page');
+
+
+    const lastScore = getElement('score');
+    setElement('last-score', lastScore);
+
+
+    const cAlphabet = getElementText('alphabet-screen');
+    removeBackgroundColor(cAlphabet);
 }
